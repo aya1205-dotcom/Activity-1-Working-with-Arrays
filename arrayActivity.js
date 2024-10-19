8b911a95d0e6282916c050392a04d943fb0dcf0c
angelettecortez <cortezangelette@gmail.com>
2024-10-19 13:05:45 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
+// Favorite Singers
+let favSingers = ["Arthur Nery", "Chris Brown", "Hev Abi"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["11", "12", "03", "05"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
